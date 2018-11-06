b1cbcd9e747352c0983a66c6d973e696b1815496
Kripanshu Bhargava <kripanshubhargava@gmail.com>
2018-11-06 16:10:04 -0600

    structre part 1

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://krip:krips123@ds155293.mlab.com:55293/devconnector94'
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const profile = require('./routes/api/profile')
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
+
+const app = express();
+
+// config DB
+const db = require('./config/keys').mongoURI;
+
+// connect to mongoose
+mongoose.connect(db).then(() => console.log('MongoDB Connected'))
+.catch(err => console.log("Error in connceting to MongoDB ", err));
+
+app.get('/', (req, res) => res.send("Forca Barca"));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server running on port ', port));

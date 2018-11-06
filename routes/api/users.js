7cba3e37623f84998688c5ae28f20b0e0cf16be4
Kripanshu Bhargava <kripanshubhargava@gmail.com>
2018-11-06 17:00:10 -0600

    user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,71 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+// Load user model
+const User = require('../../models/User');
+
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public 
-
 router.get('/test', (req, res)=>{
   res.json({msg: "Users works"});
 });
+
+// @route   GET api/users/register
+// @desc    Register users route
+// @access  Public 
+router.post('/register', (req, res)=>{
+  User.findOne({email: req.body.email})
+  .then(user => {
+    if(user){
+      return res.status(400).json({email: 'Email already exists'});
+    }else{
+      const avatar = gravatar.url(req.body.email,{
+        s:'200',//size
+        r:'pg', // Rating
+        d:'mm' //Default
+      })
+      const newUser = new User({
+        name:req.body.name,
+        email:req.body.email,
+        avatar,
+        password:req.body.password
+      });
+
+      bcrypt.genSalt(10,(err, salt)=>{
+        bcrypt.hash(newUser.password, salt, (err, hash) =>{
+          if (err) throw err;
+            newUser.password = hash;
+            newUser.save().then(user  =>  res.json(user))
+            .catch(err=> console.log("Error : ", err));
+      
+        })
+      });
+    }
+  });
+});
+
+// @route   GET api/users/login
+// @desc    Login users route
+// @access  Public 
+router.post('/login',(req, res)=>{
+  const email = req.body.email;
+  const password = req.body.password;
+  // find user
+  User.findOne({email}).then(user => {
+    if(!user){
+      return res.status(400).json({email: 'user not found'});
+    }
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch =>{
+      if(isMatch){res.json({msg: 'Success'})}
+      else{
+        return res.status(400).json({password: 'Incorrect Password | Does not match'});
+      }
+    });
+  });
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const profile = require('./routes/api/profile')
 const users = require('./routes/api/users')
 const posts = require('./routes/api/posts')
 
 const app = express();
+// Body Parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // config DB
 const db = require('./config/keys').mongoURI;
